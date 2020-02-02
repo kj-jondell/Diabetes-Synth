0d2002e317f3b741c20abeac1d829d73a27a797e
kj-jondell <kj@jondell.com>
2020-02-02 12:33:06 +0100

    new glucose levels

--- python/blodsocker.py
+++ python/blodsocker.py
@@ -1,46 +1,57 @@
 ### Interpreter of blood glucose data from Abbot FreeStyle Libre. Spline interpolation and ouptut of 1024 sample wav-file.
 ### TODO: 1. Cleanup code
-### 2. automation
+### 2. automation (DONE)
 ### 3. improve date interpolation (include year for example)
 
 from dateutil import parser
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate
+from scipy.interpolate import BSpline
 from scipy import signal
+import scipy
 import soundfile as sf
 
 SAMPLE_RATE = 48000
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 2048
+IS_WAVETABLE = True #Supercollider wavetable format
+AMT_OUTPUT = 30
 
+days_per_month = [31,28,31,30,31,30,31,31,30,31,30,31]
 times = []
 values = []
-with open('python/blodsocker.txt', 'r') as reader:
+with open('python/blodsocker.txt', 'r', encoding='utf-8-sig') as reader:
     ### read and interpret data.
     lines = reader.readlines()
     amtData = len(lines)
+    baseTime = 0
     for line in lines:
         splitline = line.split()
         date = parser.parse(splitline[0] + " " + splitline[1])
         values.append(float(splitline[2]))
-        times.append(date.month*60*744+date.day*60*24+date.hour*60+date.minute)
+        year = date.year-2019
+        time = (year*365+sum(days_per_month[0:(date.month-1)])+date.day)*60*24+date.hour*60+date.minute
+        if baseTime == 0:
+            baseTime = time
+        times.append(time-baseTime)
 
-    ### interpolation
-    times = [int(x - times[0]) for x in times]
-    tck = interpolate.splrep(times[0:BUFFER_SIZE], values[0:BUFFER_SIZE], k=1)
+    spl = BSpline(times, values, k=1)
 
     ### output extraction
-    amtOuptut = 15
-    for sampleIndex in range(0,amtOuptut):
+    for sampleIndex in range(0,AMT_OUTPUT):
         sample = []
         for x in range(BUFFER_SIZE*sampleIndex,BUFFER_SIZE*(sampleIndex+1)):
-            sample.append(interpolate.splev(x,tck))
+            sample.append(spl(x))
         sample = [(x-min(sample)) for x in sample]
         sample = [ 2*(x/max(sample)-0.5) for x in sample] #normalize and center sound
-        window = signal.tukey(BUFFER_SIZE, 0.1) #window function
+        window = signal.tukey(BUFFER_SIZE, 0.1) #window function, to smoothen buffer
         for ind, w_sample in enumerate(window):
             sample[ind] = sample[ind]*w_sample
-
-        sf.write('samples/blodsocker{}.wav'.format(sampleIndex+1), sample, SAMPLE_RATE)
+        if IS_WAVETABLE:
+            for ind, c_sample in enumerate(sample):
+                if ind % 2 == 0:
+                    sample[ind] = 2*sample[ind] - sample[ind+1]
+                else:
+                    sample[ind] = sample[ind] - sample[ind-1]
+        sf.write('samples/wavetable2048/blodsocker{}.wav'.format(sampleIndex+1), sample, SAMPLE_RATE)
 
 
--- supercollider/wavetable.sc
+++ supercollider/wavetable.sc
@@ -18,7 +18,8 @@ s.options.device = "Soundflower (64ch)"
 * ® Karl Johannes Jondell
 */
 
-b = SoundFile.collectIntoBuffers("/Users/kj/Documents/school/supercollider/experiments/diabetes/samples/*")
+b = SoundFile.collectIntoBuffers("/Users/kj/Documents/school/supercollider/projects/diabetes/samples/*") //No interpolation
+b = SoundFile.collectIntoBuffers("/Users/kj/Documents/school/supercollider/projects/diabetes/samples/wavetable/*") //WAVETABLE interpolation
 
 //GRANULAR
 (
@@ -29,23 +30,30 @@ b = SoundFile.collectIntoBuffers("/Users/kj/Documents/school/supercollider/exper
             var sigL = BufGrain.ar(t_trig, rel, bufL, rate: freq/110); //Can only control rate(!)
             var sigR = BufGrain.ar(t_trig, rel, bufR, rate: freq/110*1.01); //Can only control rate(!)
             var env = EnvGen.kr(Env.perc(attackTime: 1, releaseTime:rel), doneAction: Done.freeSelf);
-            Out.ar(outL,0.7*env*[sigL,sigR]);
+            Out.ar(outL,0.1*env*[sigL,sigR]);
         }
     ).add;
 )
-//WAVETABLE (non-interpolating)
+//WAVETABLE (interpolating)
 (
     SynthDef.new(
         \diabetes,
         {
-            arg freq = 440, rel = 0.1, bufL = 1, bufR = 2, outL = 0, outR = 1;
-            var sigL = OscN.ar(bufL, freq);
-            var sigR = OscN.ar(bufR, freq*1.01);
-            var env = EnvGen.kr(Env.perc(releaseTime:rel), doneAction: Done.freeSelf);
-            Out.ar(outL,env*[sigL,sigR]);
+            arg freq = 440, velocity = 67, attack = 0.01, rel = 0.1, buf = 1, /*bufR = 2, outL = 0, outR = 1,*/ pan = 0;
+            var sig = VOsc.ar(buf, freq:freq, mul: velocity/150);
+            //var sigR = OscN.ar(bufR, freq*1.01);
+            var env = EnvGen.kr(Env.perc(attackTime: attack, releaseTime:rel), doneAction: Done.freeSelf);
+            var filter = LPF.ar((env*sig), freq*velocity.linlin(0,127,0.75,12));
+            Out.ar(pan,filter);
         }
     ).add;
 )
+
+//DIABETES DRONE
+x = Synth.new(\diabetes, [\attack,10,\rel,100,\velocity,120,\pan,2,\freq,220,\buf,Rand(1,14)])
+x = Synth.new(\diabetes, [\attack,10,\rel,200,\velocity,60.rand+60,\pan,10.rand,\freq,154.1,\buf,14.0.rand])
+x = Synth.new(\diabetes, [\attack,10,\rel,200,\velocity,60.rand+60,\pan,1,\freq,220,\buf,14.0.rand])
+
 //TEST PBIND
 (
     Pbindef.new(
@@ -58,9 +66,9 @@ b = SoundFile.collectIntoBuffers("/Users/kj/Documents/school/supercollider/exper
         \bufR, Prand([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], inf), 
         \outL, Prand([0,1,2,3,4,5,6], inf),
         \scale, Scale.whole,
-        \rel, Pwhite(2,5,inf),
+        \rel, Pwhite(5,10,inf),
         \dur, Pxrand([1/2,1/3,1/8]/1, repeats:inf)
-    ).play;
+    ).play(quant:4);
 )
 
 
