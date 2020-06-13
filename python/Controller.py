4e21fed48e8320b232d89a2600c68041d85da7e4
kj-jondell <kj@jondell.com>
2020-06-13 18:03:13 +0200

    working tool

--- python/Controller.py
+++ python/Controller.py
@@ -3,15 +3,17 @@ from PySide2.QtWidgets import (QApplication, QLabel, QPushButton,
                                QVBoxLayout, QWidget, QFileDialog)
 from PySide2.QtCore import Slot, Qt, QFile, QIODevice
 from PySide2.QtUiTools import QUiLoader
+from Model import Model
 
-UI_FILE_NAME = "ui/controller.ui" # Qt Designer ui file
+UI_FILE_NAME = "python/ui/controller.ui" # Qt Designer ui file
 
-class View(QApplication):
+class Controller(QApplication):
 
     def __init__(self):
         QApplication.__init__(self)
         self.load_view()
         self.window.file_chooser.clicked.connect(self.choose_file)
+        self.window.run_button.clicked.connect(self.convert_files)
 
     def load_view(self):
         self.ui_file = QFile(UI_FILE_NAME)
@@ -27,10 +29,24 @@ class View(QApplication):
             print(self.loader.errorString())
             sys.exit(-1)
 
+    def convert_files(self):
+        sample_rate = (int)(self.window.sample_rate.currentText()) 
+        buffer_size = (int)(self.window.buffer_size.currentText())
+        window_size = self.window.window_size.value() 
+        is_wavetable = self.window.is_wavetable.isChecked() 
+        write_file = self.window.write_files.isChecked() 
+        window_type = self.window.window_type.currentText() 
+        amt_output = self.window.amt_files.value() 
+
+        Model(self.chosen_filename, sample_rate, buffer_size,
+                window_size, is_wavetable, write_file,
+                window_type, amt_output)
+
     def choose_file(self):
-        self.chosen_filename = QFileDialog.getOpenFileName(self.window, 'Open file', filter = "XLS files (*.xls)")
-        if self.chosen_filename[0] != "":
-            self.window.filename.setText(self.chosen_filename[0])
+        self.chosen_filename, filter_type = QFileDialog.getOpenFileName(self.window, 'Open file', filter = "XLS files (*.xls)")
+
+        if self.chosen_filename != "":
+            self.window.filename.setText(self.chosen_filename)
             self.change_enabled_settings()
 
     # TODO group objects that are settings control instead of hardcoded hack !!!
@@ -46,4 +62,4 @@ class View(QApplication):
         self.window.show()
         sys.exit(self.exec_())
 
-View().run_view()
+Controller().run_view()
--- python/Model.py
+++ python/Model.py
@@ -5,6 +5,7 @@
 ### 3. GUI! (MVC)
 ### 4. camelCase to snake_case...
 
+from dataclasses import dataclass
 from dateutil import parser
 import numpy 
 from scipy.interpolate import BSpline
@@ -20,27 +21,35 @@ BUFFER_SIZE = 2048
 WINDOW_SIZE = 0.1
 IS_WAVETABLE = False #Supercollider wavetable format
 WRITE_FILE = True
-WINDOW_TYPE = "Boxcar"
-AMT_OUTPUT = 30
+WINDOW_TYPE = "Hann"
+AMT_OUTPUT = 3
 
 # CALIBRATION
 ROW_OFFSET = 5
 SHEET_NO = 1
 WINDOWS = {"Tukey" : signal.tukey, "Hamming" : signal.hamming, "Hann" : signal.hann, "Boxcar" : signal.boxcar, "Blackman" : signal.blackman} # TODO fix parameters for each type of window
 
+@dataclass
 class Model:
+    filename: str
+    sample_rate: int = SAMPLE_RATE
+    buffer_size: int = BUFFER_SIZE
+    window_size: float = WINDOW_SIZE
+    is_wavetable: bool = IS_WAVETABLE
+    write_file: bool = WRITE_FILE
+    window_type: str = WINDOW_TYPE
+    amt_output: int = AMT_OUTPUT
 
-    def __init__(self, filename = "blodsocker.xls", sample_rate = SAMPLE_RATE, 
-            buffer_size = BUFFER_SIZE, window_size = WINDOW_SIZE, 
-            is_wavetable = IS_WAVETABLE, write_file = WRITE_FILE, 
-            signal_type = WINDOW_TYPE, amt_output = AMT_OUTPUT):
-
-        self.window = WINDOWS[WINDOW_TYPE](BUFFER_SIZE, WINDOW_SIZE if WINDOW_TYPE == "Tukey" else None) #window function, to smoothen buffer TODO make selectable
-        self.times = []
-        self.values = []
+    def __post_init__(self):
+        self.window = WINDOWS[self.window_type](self.buffer_size, self.window_size if self.window_type == "Tukey" else None) #window function, to smoothen buffer TODO make selectable
 
         self.centroids = list()
-        self.sheet = xlrd.open_workbook(filename).sheet_by_index(SHEET_NO) 
+        self.sheet = xlrd.open_workbook(self.filename).sheet_by_index(SHEET_NO) 
+
+        self.times, self.values = self.parse_data(self.sheet)
+        self.spl = BSpline(self.times, self.values, k = 1)
+
+        self.extract_output(self.spl)
 
     #function taken from https://stackoverflow.com/questions/54032515/spectral-centroid-of-numpy-array
     def spectral_centroid(self, x, samplerate = 44100):
@@ -50,11 +59,12 @@ class Model:
         magnitudes = magnitudes[:length//2+1]
         return numpy.sum(magnitudes*freqs) / numpy.sum(magnitudes)
 
-
-amt_files = sheet.nrows - ROW_OFFSET
-    def get_times(amt_files, sheet):
+    def parse_data(self, sheet):
+        amt_data = sheet.nrows - ROW_OFFSET
         base_time = 0
-        for row in range(amt_files):
+        times = []
+        values = []
+        for row in range(amt_data):
             ### read and interpret data.
             date_string = sheet.cell_value(rowx = ROW_OFFSET + row, colx = 0)
             glucose_level = sheet.cell_value(rowx = ROW_OFFSET + row, colx = 1)
@@ -65,29 +75,32 @@ amt_files = sheet.nrows - ROW_OFFSET
             if base_time == 0:
                 base_time = date
             times.append((int)((date-base_time).total_seconds()/60))
-    return times
+        return times, values
 
-spl = BSpline(times, values, k=1)
+    ### output extraction
+    def extract_output(self, spl):
+        for sample_index in range(0, self.amt_output):
+            sample = []
+            for x in range(self.buffer_size*sample_index, self.buffer_size*(sample_index+1)):
+                sample.append(spl(x))
+            sample = [(x-min(sample)) for x in sample]
+            sample = [ 2*(x/(max(sample)-min(sample))-0.5) for x in sample] #normalize and center sound
 
-### output extraction
-for sampleIndex in range(0, AMT_OUTPUT):
-    sample = []
-    for x in range(BUFFER_SIZE*sampleIndex,BUFFER_SIZE*(sampleIndex+1)):
-        sample.append(spl(x))
-    sample = [(x-min(sample)) for x in sample]
-    sample = [ 2*(x/(max(sample)-min(sample))-0.5) for x in sample] #normalize and center sound
+            for ind, w_sample in enumerate(self.window):
+                sample[ind] = sample[ind]*w_sample
+            if self.is_wavetable:
+                for ind, c_sample in enumerate(sample):
+                    if ind % 2 == 0:
+                        sample[ind] = 2*sample[ind] - sample[ind+1]
+                    else:
+                        sample[ind] = sample[ind] - sample[ind-1]
 
-    for ind, w_sample in enumerate(window):
-        sample[ind] = sample[ind]*w_sample
-    if IS_WAVETABLE:
-        for ind, c_sample in enumerate(sample):
-            if ind % 2 == 0:
-                sample[ind] = 2*sample[ind] - sample[ind+1]
-            else:
-                sample[ind] = sample[ind] - sample[ind-1]
+            self.centroids.append(self.spectral_centroid(sample, self.sample_rate)) #prints spectral centroids (TODO: print as list)
+            if self.write_file:
+                sf.write('blodsocker{}.wav'.format(sample_index+1), sample, self.sample_rate) #ask for name
 
-    centroids.append(spectral_centroid(sample, SAMPLE_RATE)) #prints spectral centroids (TODO: print as list)
-    if WRITE_FILE:
-        sf.write('blodsocker{}.wav'.format(sampleIndex+1), sample, SAMPLE_RATE)
-
-print(centroids)
+# print(self.centroids)
+# Model("python/sketches/blodsocker.xls") # TEMPORARY
+# Model("python/sketches/blodsocker.xls", SAMPLE_RATE,
+# BUFFER_SIZE, WINDOW_SIZE, IS_WAVETABLE,
+# WRITE_FILE,  WINDOW_TYPE, AMT_OUTPUT)
