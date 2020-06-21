3eb9b49be4f533c25c0d32dd5c67ec3175a85ab7
kj-jondell <kj@jondell.com>
2020-06-21 22:32:37 +0200

    supercollider changes

--- wavetable-synth/python/Interface.py
+++ wavetable-synth/python/Interface.py
@@ -9,7 +9,8 @@ import csv, pathlib
 
 SETTINGS_FILE = str(pathlib.Path(__file__).parent / "../../settings") #(move settings to shared module)
 UI_FILE_NAME = "wavetable_gui.ui" # Qt Designer ui file TODO fix path!
-WIDGETS_PAIRS = [('memory_size', 'numframes'), ('sample_rate', 'samplerate')]
+WIDGETS_PAIRS = [('buffer_size', 'numframes'), ('sample_rate', 'samplerate'), ('memory_size', 'memsize')]
+WIDGET_LOAD_SEPARATELY = [('output_devices', 'device')]
 
 #TODO change these to shared module
 def read_settings(path):
@@ -18,6 +19,12 @@ def read_settings(path):
         reader = csv.reader(csvfile)
         settings_dict = {row[0]:row[1][1:] for row in reader}
     return settings_dict 
+
+def write_settings(settings_dict, path):
+    with open(path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for value in settings_dict:
+            writer.writerow([value, " "+ settings_dict[value]])
 
 # TODO group objects that are settings control instead of hardcoded hack !!!
 def change_enabled_settings(widgets, exceptions = ["file_chooser"], enable = True):
@@ -33,18 +40,26 @@ class Interface(QMainWindow):
     def __init__(self, parent = None):
         QMainWindow.__init__(self)
 
-        self.default_settings = read_settings(SETTINGS_FILE)
+        self.settings = read_settings(SETTINGS_FILE)
 
         self.load_view()
-        self.load_output_devices(self.default_settings['device'])
+        self.load_output_devices(self.settings['device'])
         self.load_settings()
 
         self.central_widget.load_project.clicked.connect(self.choose_project)
+        self.central_widget.run_button.clicked.connect(self.start_synth)
 
         self.setWindowTitle("Wavetable Synthesizer")
         self.setCentralWidget(self.central_widget)
 
         self.show()
+
+    def start_synth(self):
+        ### Chosen settings from user input
+        for name, value in WIDGETS_PAIRS + WIDGET_LOAD_SEPARATELY:
+            widget = getattr(self.central_widget, name)
+            self.settings[value] = widget.currentText()
+        write_settings(self.settings, SETTINGS_FILE)
 
     def choose_project(self):
         self.chosen_project, filter_type = QFileDialog.getOpenFileName(self.central_widget, 'Open file', filter = "DIA project (*.dia)")
@@ -53,8 +68,8 @@ class Interface(QMainWindow):
             self.central_widget.project_name.setText(pathlib.Path(self.chosen_project).stem)
             change_enabled_settings(self.central_widget, exceptions = ["load_project"])
 
-            settings = read_settings(self.chosen_project)
-            self.default_settings = {**self.default_settings, **settings} # merge setting dictionaries
+            imported_settings = read_settings(self.chosen_project)
+            self.settings = {**self.settings, **imported_settings} # merge setting dictionaries
             self.load_settings()
 
     def load_view(self):
@@ -71,7 +86,6 @@ class Interface(QMainWindow):
             print(self.loader.errorString())
             sys.exit(-1)
 
-    #TODO read from settings file
     def load_output_devices(self, default_device):
         output_devices = self.central_widget.output_devices
         for device in sounddevice.query_devices():
@@ -87,7 +101,11 @@ class Interface(QMainWindow):
 
     def load_setting(self, widget_name, value):
         widget = getattr(self.central_widget, widget_name)
-        default_value = self.default_settings[value]
+
+        if value not in self.settings: # instead of catching KeyErrors
+            return
+
+        default_value = self.settings[value]
         for index in range(widget.count()):
             if widget.itemText(index) == default_value:
                 widget.setCurrentIndex(index)
